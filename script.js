faa02503ddc203e4403ec78167da9a43ba88b8d5
Aleksander <aleksander.karatajev@gmail.com>
2022-04-07 16:00:07 +0300

    Setup the intial functions

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+const choices = ["rock", "paper", "scissors"];
+
+// plays a game of rock-paper-scissors 5 times
 function game() {
-  // plays a game of rock-paper-scissors 5 times
+  playRound();
 }
 
+// plays a single round of rock-paper-scissors
 function playRound() {
-  const playerSelection =
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
 }
 
 // get input from player
-function playerChoice () {
-
+function playerChoice() {
+  let input = prompt("Choose Rock, Paper or Scissors");
+  while (input == null) {
+    input = prompt("Choose Rock, Paper or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  if (check == true) {
+    console.log(input);
+  }
 }
 
 // get random input from computer
-function computerChoice () {
-
-
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+game();
