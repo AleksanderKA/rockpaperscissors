40b9c2dbe456cae3579f0bb4f094be768a119258
Aleksander <aleksander.karatajev@gmail.com>
2022-04-07 15:19:41 +0300

    Added script file, set up initial functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function game() {
+  // plays a game of rock-paper-scissors 5 times
+}
+
+function playRound() {
+  const playerSelection =
+}
+
+// get input from player
+function playerChoice () {
+
+}
+
+// get random input from computer
+function computerChoice () {
+
+
+}
+
